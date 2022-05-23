d916d8f52b623b2df396053e198f64bdaf24b818
Nick Carver <nick.carver@rusticisoftware.com>
2022-05-23 15:46:46 -0500

    migrated once and corrected the syntax in the urls.py file

--- TestRestAPI/urls.py
+++ TestRestAPI/urls.py
@@ -24,4 +24,6 @@ router.register(r'responses', ResponseViewSet, 'response')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
